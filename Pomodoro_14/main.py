d08dd5658fa702518844710b6d1cc0c740c4eac3
Muharrem <123410332+MuharremK0@users.noreply.github.com>
2023-09-18 09:57:25 +0300

    Update main.py

--- Pomodoro_14/main.py
+++ Pomodoro_14/main.py
@@ -82,7 +82,7 @@ btn2=Button(text="Reset",highlightthickness=0,command=reset_timer)
 btn2.grid(column=2,row=2)
 
 canvas=Canvas(width=200,height=224,bg=YELLOW,highlightthickness=0)
-tomato_img=PhotoImage(file="./day_28/tomato.png")
+tomato_img=PhotoImage(file="./Pomodoro_14/tomato.png")
 canvas.create_image(100,112,image=tomato_img)
 timer_text=canvas.create_text(100,130,text="00.00",fill="white",font=(FONT_NAME,35,"bold"))
 canvas.grid(column=1,row=1)
